c4e20d289a3b286b9c201ca67825b04409ff7c72
Jacob Cardon <156040430+AceVRoger@users.noreply.github.com>
2025-01-22 21:07:45 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+function MakeDark() {
+            document.getElementById('body').style.backgroundColor = "black";
+            document.getElementById('lighting').style.color = "red";
+            document.getElementById('lighting').style.backgroundColor = "black";
+            document.getElementById('text').style.color = "red";
+            document.getElementById('text').style.backgroundColor = "black";
+            document.getElementById('projects').style.color = "red";
+            document.getElementById('projects').style.backgroundColor = "black";
+            document.getElementById('header1').style.backgroundColor = "grey";
+            document.getElementById('header2').style.backgroundColor = "grey";
+            document.getElementById('header6').style.backgroundColor = "grey";
+        }
+
+        function MakeDefault() {
+            document.getElementById('body').style.backgroundColor = "red";
+            document.getElementById('lighting').style.color = "yellow";
+            document.getElementById('lighting').style.backgroundColor = "red";
+            document.getElementById('text').style.color = "yellow";
+            document.getElementById('text').style.backgroundColor = "red";
+            document.getElementById('projects').style.color = "yellow";
+            document.getElementById('projects').style.backgroundColor = "red";
+            document.getElementById('header1').style.backgroundColor = "gold";
+            document.getElementById('header2').style.backgroundColor = "gold";
+            document.getElementById('header6').style.backgroundColor = "gold";
+        }
+
+        function MakePink() {
+            document.getElementById('body').style.backgroundColor = "#ffb7c5";
+            document.getElementById('lighting').style.color = "#e0218a";
+            document.getElementById('lighting').style.backgroundColor = "#ffb7c5";
+            document.getElementById('text').style.color = "#e0218a";
+            document.getElementById('text').style.backgroundColor = "#ffb7c5";
+            document.getElementById('projects').style.color = "#e0218a";
+            document.getElementById('projects').style.backgroundColor = "#ffb7c5";
+            document.getElementById('header1').style.backgroundColor = "#db7093";
+            document.getElementById('header2').style.backgroundColor = "#db7093";
+            document.getElementById('header6').style.backgroundColor = "#db7093";
+        }
