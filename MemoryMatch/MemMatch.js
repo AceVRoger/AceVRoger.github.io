0cec84e8ba6973eb063533f21f529b72906c3a4d
Jacob Cardon <jacobcardon@ad.weber.edu>
2025-02-17 20:26:57 -0700

    adding memory match game and photos

---
+++ MemoryMatch/MemMatch.js
@@ -0,0 +1,119 @@
+let map = [];
+let tile1 = null, tile2 = null;
+let matchedCount = 0; // count of matched pairs
+
+// TODO: write the project code
+const loadMap = (rows = 4, cols = 4) => {
+    //Ensuring the tiles left are even when/If I change the default settings
+    if ((rows * cols) % 2 !== 0) {
+        console.error('Error in tile count (Not Even Count).')
+        return;
+    };
+
+    // calculating max value of pairs
+    const maxValue = (rows * cols) / 2;
+
+    // array to hold tile pairs
+    const pairs = [];
+    for (let i = 1; i <= maxValue; i++) {
+        pairs.push(i, i); // should make each number appear twice?
+    };
+
+    //shuffling
+    const shuffledPairs = shuffle(pairs);
+    console.log(shuffledPairs);
+
+    let titleIndex = 0;
+    map = []; // Resetting map
+
+    // 2D-Array with the shuffled pairs
+    for (let r = 0; r < rows; r++) {
+        const row = [];
+        for (let c = 0; c < cols; c++) {
+            row.push(shuffledPairs[titleIndex]);
+            titleIndex++;
+        }
+        map.push(row);
+    }
+    console.log(map);
+};
+
+const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+};
+
+const makeBoard = (rows = 4, cols = 4) => {
+    // Make sure map loads...
+    if (map.length === 0) {
+        console.error("Map didn't load... Error");
+        return;
+    }
+
+    let output = '<table>';
+    for (let row = 0; row < rows; row++) {
+        output += '<tr>';
+        for (let col = 0; col < cols; col++) {
+            output += `<td><button class="tile" id="${row}_${col}" onclick="process(${row}, ${col})"></button></td>`;
+    }
+    output += '</tr>';
+}
+document.getElementById('gameBoard').innerHTML = output + '</table>';
+};
+
+// CLicked Tiles:
+const process = (row = 4, col = 4) => {
+    let clickedTile = document.getElementById(row + '_' + col);
+
+    // Clicked tile not already contains data
+    if (!Boolean(clickedTile.innerHTML)){
+
+        // Tile 1 not yet selected
+        if (!tile1) {
+            tile1 = clickedTile;
+            clickedTile.innerHTML = map[row][col];
+
+        }
+        // Tile 2 not yet selected
+        else if (!tile2) {
+            tile2 = clickedTile;
+            clickedTile.innerHTML = map[row][col];
+
+            // TODO: Set timeout callback function
+            setTimeout(checkMatch, 1000);
+        }
+    }
+};
+
+const checkMatch = () => {
+
+    if (tile1.innerHTML === tile2.innerHTML) {
+        // If tiles match, hide them
+        tile1.hidden = true;
+        tile2.hidden = true;
+        matchedCount += 1;
+
+        if (matchedCount === (map.length * map[0].length) / 2) {
+            alert("Congrats! Every Tile Has A Match!!!")
+        }
+    }
+
+    // resetting tiles if they didn't match
+    tile1.innerHTML = '';
+    tile2.innerHTML = '';
+    tile1 = tile2 = null;
+};
+
+const resetBoard = () => {
+    loadMap(4, 4);
+    makeBoard(4, 4);
+    tile1 = tile2 = null;
+    matchedCount = 0;
+};
+
+resetBoard();
--- inClassAssignments/MFD.js
+++ inClassAssignments/MFD.js
@@ -147,4 +147,134 @@ const addDear = name => "Dear" + name;
 const greetedStudents = students.map(addDear);
 console.log(greetedStudents);
 
+// Example #2 (map)
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = function(number) {
+    return number * number;
+}
+
+const squaredNumbers = numbers.map(squares);
+const squaredNumbers = numbers.map( function(number) {
+    return number * number;
+});
+const squaredNumbers = numbers.map(number => number * number);
+
+console.log(squaredNumbers);
+
+// .filter() - new array with elements that pass those conditions
+const usernames = ['John1234MySite', 'AntonioOrlandoFernando', 'Lancelot', 'TinyTimABC5'];
+
+//const longNames = function (name) {
+//    return name.length > 10;
+//}
+//
+//const longUsernames = usernames.filter(longNAmes);
+//console.log(longUsernames);
+
+const longUsernames = usernames.filter(name => name.length > 10);
+console.log(longUsernames);
+
+// .reduce() - iterates an array and accumulates into a single result
+//const sumFunction = function(accumulator, currentValue) {
+//    console.log(accumulator + ', ' + currentValue)
+//    return accumulator + currentValue;
+//}
+//
+//const total = numbers.reduce(sumFunction, 0); // can also add a starting point where the 0 is.
+//console.log(total);
+
+const total = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(total);
+
+// .forEach()
+//const items = ['item1', 'item2', 'item3'];
+//
+//const decorate = item => {
+//    console.log('|' + item + '|')
+//}
+//
+//items.forEach(decorate);
+
+items.forEach(item => console.log('|' + item + '|'));
+
 // Examples we made
+
+function repeatAction(action, times) {
+    for (let i = 0; i <= times; i++) {
+        action(i);
+    }
+}
+
+const elmoCount = function(number) {
+    console.log('Elmo says', number + '!');
+}
+
+const sayHello = function(unimportant) {
+    console.log('Hello');
+}
+
+repeatAction(elmoCount, 10);
+repeatAction(sayHello, 5);
+
+const applyOperation = function(a, b, operation) {
+    return operation(a, b);
+}
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+let outcome = applyOperation(5, 3, add);
+console.log(outcome);
+
+let outcome2 = applyOperation(5, 33, multiply);
+console.log(outcome2);
+
+// Function as a return value; creates a brand new function
+function createGreeter(message) {
+    return function(name){
+        return message + ', ' + name + '!';
+    }
+}
+
+const greetMorning = createGreeter('Good Morning');
+console.log(greetMorning('Samantha'));
+console.log(greetMorning('Jacob'));
+
+const greetAfternoon = createGreeter('Good Afternoon');
+console.log(greetAfternoon('Sam'));
+console.log(greetAfternoon('Will'));
+
+// Functions as an argument
+// Callback: a function passed as an argument to another function. the oter function can "callback" the passed in function at a later point in time.
+
+// Common use: ensuring one function finishes before another function runs.
+function sayHelloAlert() {
+    alert('Hello!');
+}
+
+setTimeout(sayHelloAlert, 9000);
+setTimeout(createGreeter('Hello'), 5000);
+
+
+// Functions as a return
+// Closures: function return where the variables maintain their scope
+function createCounter() {
+    let count = 0;
+    return function() {
+        count++;
+        return count; // reminder as to why coding count++ on this row wouldn't work.
+    }
+}
+
+const imageCounter = createCounter();
+console.log(imageCounter());
+console.log(imageCounter());
+
+// Minesweeper game
+// 2D arrays
